40cf340b76c16a5f578d4eb51c6d33fe0ab0c742
haider1837 <57004907+haider1837@users.noreply.github.com>
2020-01-31 11:20:00 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+var Discord = require('discord.js');
+var fs = require('fs');
+var client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Welcome Bro ${client.user.tag}!`);
+});
+var prefix = '#'
+
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "432662769809096705") return;
+
+  
+  if (message.content.startsWith(prefix + 'setwatch')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}`)
+} 
+
+ 
+  if (message.content.startsWith(prefix + 'setlis')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
+} 
